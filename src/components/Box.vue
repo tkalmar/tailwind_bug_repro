ca3b64b6b3711ab2a735b6ad672cbb4e7f7f90bf
Thomas Kalmár <puma.rsch@gmail.com>
2023-02-12 16:59:44 +0100

    Initial commit

---
+++ src/components/Box.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div v-if="title" class="bg-red-900">{{title}}</div>
+        <div v-else class="default_title">Default title</div>
+        <div> I'm a box</div>
+    </div>
+</template>
+<script setup lang="ts">
+const props = defineProps({
+    title: {
+        type: String,
+        default: ''
+    }
+})
+
+</script>
+<style lang="postcss" scoped>
+.default_title {
+    @apply text-xl bg-green-400 border-orange-600;
+}
+</style>
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Box from "./components/Box.vue";
+import './style.css'
+
+export {
+    Box,
+}
